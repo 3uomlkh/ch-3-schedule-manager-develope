95835fd2d397972b06487f1b5bfa7795428766dc
3uomlkh <3uomlkh@gmail.com>
2025-02-13 10:58:37 +0900

    fix: Validation 어노테이션 오류 수정

--- src/main/java/com/example/schedulemanagerdevelop/dto/request/UpdateUsernameRequestDto.java
+++ src/main/java/com/example/schedulemanagerdevelop/dto/request/UpdateUsernameRequestDto.java
@@ -1,14 +1,14 @@
 package com.example.schedulemanagerdevelop.dto.request;
 
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class UpdateUsernameRequestDto {
 
     @NotBlank(message = "이름은 필수값입니다.")
-    @Max(value = 8, message = "이름은 8자 이내여야 합니다.")
+    @Size(min = 1, max = 10, message = "이름은 10자 이내여야 합니다.")
     private String username;
 
 }
